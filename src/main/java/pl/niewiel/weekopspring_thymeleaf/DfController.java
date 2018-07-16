7fe7c4dfdac7f923a7cec5b83e1debe3ef84ab0e
Tomasz Niewielski <tniewiel@gmail.com>
2018-07-16 19:01:40 +0200

    Initial commit

---
+++ src/main/java/pl/niewiel/weekopspring_thymeleaf/DfController.java
@@ -0,0 +1,8 @@
+package pl.niewiel.weekopspring_thymeleaf;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DfController {
+
+}
---
+++ src/main/java/pl/niewiel/weekopspring_thymeleaf/WeekopSpringThymeleafApplication.java
@@ -0,0 +1,12 @@
+package pl.niewiel.weekopspring_thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekopSpringThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeekopSpringThymeleafApplication.class, args);
+    }
+}
